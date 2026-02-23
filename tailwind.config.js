c40671abc76a4a486e99e7deee42968050b1dbea
Muhammad Tabish Nadeem <72700036+chtabish12@users.noreply.github.com>
2026-02-23 19:48:03 +0000

    fix tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
-'module.exports = {
-purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-    theme: {
-  extend: { },
-},
-variants: {
-  extend: { },
-},
-plugins: [],
-};
-'
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // if using Next.js app router
+  ],
+  darkMode: 'class', // or 'media' or false
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
