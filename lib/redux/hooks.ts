f3e27f8cb5fc00cafe8c035602e54b0f40a54339
Muhammad Tabish Nadeem <72700036+chtabish12@users.noreply.github.com>
2026-02-23 18:22:43 +0000

    Update hooks file

--- lib/redux/hooks.ts
+++ lib/redux/hooks.ts
@@ -1,6 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../store'; // Adjust the import path accordingly
+import type { AppDispatch, RootState } from './store';
 
-// Typed versions of useDispatch and useSelector
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
