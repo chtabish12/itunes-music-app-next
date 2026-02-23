d97eea81224a7845779783b7693017704c4a3098
Muhammad Tabish Nadeem <72700036+chtabish12@users.noreply.github.com>
2026-02-23 18:11:48 +0000

    Create home page component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to iTunes Music App</h1>
+      <p>Discover your favorite music!</p>
+    </div>
+  );
+};
+
+export default HomePage;
